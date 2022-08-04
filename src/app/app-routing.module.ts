968378c9380fd2d3479aea85a0492052d02c61cc
Dániel Boros <daniel.boros.17@gmail.com>
2022-08-04 18:18:23 +0200

    Remove demo folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { NotfoundComponent } from './demo/components/notfound/notfound.component';
+import { NotFoundComponent } from './layout/components/notfound/notfound.component';
 import { AppLayoutComponent } from "./layout/app.layout.component";
 
 @NgModule({
@@ -12,9 +12,9 @@ import { AppLayoutComponent } from "./layout/app.layout.component";
                     { path: '', loadChildren: () => import('./layout/components/datatable/datatable.module').then(m => m.DatatableModule) },
                 ],
             },
-            { path: 'auth', loadChildren: () => import('./demo/components/auth/auth.module').then(m => m.AuthModule) },
-            { path: 'pages/notfound', component: NotfoundComponent },
-            { path: '**', redirectTo: 'pages/notfound' },
+            { path: 'auth', loadChildren: () => import('./layout/components/auth/auth.module').then(m => m.AuthModule) },
+            { path: 'notfound', component: NotFoundComponent },
+            { path: '**', redirectTo: 'notfound' },
         ], { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled' })
     ],
     exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,11 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppLayoutModule } from './layout/app.layout.module';
-import { NotfoundComponent } from './demo/components/notfound/notfound.component';
-import { ProductService } from './demo/service/product.service';
-import { CountryService } from './demo/service/country.service';
-import { CustomerService } from './demo/service/customer.service';
-import { EventService } from './demo/service/event.service';
-import { IconService } from './demo/service/icon.service';
-import { NodeService } from './demo/service/node.service';
-import { PhotoService } from './demo/service/photo.service';
+import { NotFoundComponent } from './layout/components/notfound/notfound.component';
 
 @NgModule({
     declarations: [
-        AppComponent, NotfoundComponent
+        AppComponent, NotFoundComponent
     ],
     imports: [
         AppRoutingModule,
@@ -22,8 +15,6 @@ import { PhotoService } from './demo/service/photo.service';
     ],
     providers: [
         { provide: LocationStrategy, useClass: HashLocationStrategy },
-        CountryService, CustomerService, EventService, IconService, NodeService,
-        PhotoService, ProductService
     ],
     bootstrap: [AppComponent]
 })
