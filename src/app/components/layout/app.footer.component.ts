d62af22ad21988e0813fe2cdbf7750537f77c364
Dániel Boros <daniel.boros.17@gmail.com>
2022-12-01 23:57:07 +0100

    Display current version in footer

--- src/app/components/layout/app.footer.component.ts
+++ src/app/components/layout/app.footer.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { LayoutService } from '../../services/app.layout.service';
+import { environment } from '../../../environments/environment';
 
 @Component({
     selector: 'app-footer',
     templateUrl: './app.footer.component.html',
 })
 export class AppFooterComponent {
+    version = environment.VERSION;
+
     constructor(public layoutService: LayoutService) {}
 }
